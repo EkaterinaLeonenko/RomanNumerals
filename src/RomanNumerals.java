b9a42959b0c36cd9271c7b128eef84fab64d96fb
Kati <Kati@Kati-Netbook.Dlink>
2018-10-11 19:36:59 +0300

    Assignment finished

--- src/RomanNumerals.java
+++ src/RomanNumerals.java
@@ -1,8 +1,124 @@
 
 public class RomanNumerals {
 	public int convertToInteger(String romanNum) {
-		// To be Implemented
-		return 0;
+		
+
+		// BIG refactoring upon completion here - all multiple validity checks now moved into checkValidity method
+        int res = checkValidity(romanNum);
+        if (res < 0) return res; // 0 is ok - go forward
+        
+        // NOTE: All this TDD implemented checks, probably can be replaced by the following code, something like:
+        // if (!(romanNum.length()!=0 && romanNum.matches("^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$")))
+        //		return -1;
+        // Here we check for empty input and check for valid Roman numbers using standard regular expression available for them
+        // It will work, but here is nothing to learn with regard of TDD 
+		
+		// - 8 - Input is valid - calculations
+//      Tested ok and then refactored - see -9 -
+//		return convertSingleNum(romanNum);
+	
+		// - 9 - Refactoring to any number without suntractions
+		
+		int sum = 0;
+		String c;
+		
+		for (int i = 0; i < romanNum.length(); i++) {
+			c = romanNum.substring(i,i+1);
+			sum += convertSingleNum(c);
+		}
+		// Refactoring - 10 - Correct sum with necessary subtractions - which implemented in private method
+		// return sum; 
+		return sum - getSubtractions(romanNum);
 		
 	}
+	
+	private boolean hasWrongRepetition (String romanNum) {
+		if (romanNum.contains("IIII") || romanNum.contains("XXXX") || romanNum.contains("CCCC") || romanNum.contains("MMMM")) 
+			return true;
+		// Refactoring - new check added
+		else 	if (romanNum.contains("VV") || romanNum.contains("LL") || romanNum.contains("DD") ) 
+					return true;
+		 		else
+		 			return false;
+	}
+	
+	private boolean hasWrongSubtraction (String romanNum) {
+		if (romanNum.contains("IL") || romanNum.contains("IC") || romanNum.contains("ID") || 
+			romanNum.contains("IM") || romanNum.contains("XD") || romanNum.contains("XM"))
+			return true;
+		// Refactoring - new check added
+		else 	if (romanNum.contains("IIV") || romanNum.contains("IIX") || romanNum.contains("XXL") ||
+				    romanNum.contains("XXC") || romanNum.contains("CCD") || romanNum.contains("CCM")) 
+					return true;
+				// Refactoring - new check added
+				else 	if (romanNum.contains("VX") || romanNum.contains("VC") || romanNum.contains("VM") ||
+							romanNum.contains("LC") || romanNum.contains("LM") || romanNum.contains("DM")) 
+							return true;
+		 				else
+		 					return false;
+	}
+	
+	private int convertSingleNum (String str) {
+		switch(str) {
+		case "I": 
+			return 1;
+		case "V": 
+			return 5;
+		case "X": 
+			return 10;
+		case "L": 
+			return 50;
+		case "C": 
+			return 100;
+		case "D": 
+			return 500;
+		case "M": 
+			return 1000;
+		default:
+			return -8;
+		}
+	}
+	
+	private int getSubtractions(String str) {
+		int toSubtract = 0;
+		if (str.contains("IV") || str.contains("IX")) {
+			toSubtract += 1;
+		}
+		if (str.contains("XL") || str.contains("XC")) {
+			toSubtract += 10;
+		}
+		if (str.contains("CD") || str.contains("CM")) {
+			toSubtract += 100;
+		}
+		return toSubtract * 2; // * 2 - because we have already added them, so need to subtract them twice
+	}
+
+	
+	// BIG Refactoring upon completion - all validations went here from public method
+    private int checkValidity(String romanNum) {
+    	// -1- Checking if input is empty
+    	if (romanNum.isEmpty()) 
+    		return -1;          // Yes - return -1
+	
+    	// -2- Check for not valid symbols
+    	if (!romanNum.matches("[IVXLCDM]*")) // * means zero or more times
+    		return -2;                                            
+	
+    	// -3- Check for not valid repetitions IXCM
+    	//	if (romanNum.contains("IIII") || romanNum.contains("XXXX") || romanNum.contains("CCCC") || romanNum.contains("MMMM")) // check it!! 
+    	//Refactoring!		
+    	if (hasWrongRepetition (romanNum))
+    		return -3;                                              
+    	// -4- Check for not valid repetitions VLD
+    	//		if (romanNum.contains("VV") || romanNum.contains("LL") || romanNum.contains("DD") ) // check it!! 
+    	//Refactoring! Both 3 and 4 in one method now		
+    	//	    return -4;
+	
+    	//	- 5 - 6 - 7 - Subtraction Rules implemented as a method from the beginning. Refactoring is in the method  
+    	if (hasWrongSubtraction (romanNum))
+    		return -4;  
+    	
+    	return 0; // all validations ok
+    }
 }
+ 
--- tests/TestRomanNumerals.java
+++ tests/TestRomanNumerals.java
@@ -1,12 +1,103 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestRomanNumerals {
 
+	private RomanNumerals rn; // Define an object
+	
+	@Before
+	public void init() {
+		rn = new RomanNumerals(); // and create it
+	}
+	
+	
+	// Set of test cases below
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	// The very first step - check for empty input
+	public void test_empty_string() throws Exception {
+		assertEquals("Input is empty!",-1, rn.convertToInteger(""));
 	}
 
-}
+	
+	// NOTE! Valid tests 2 - to 4 refactored after implementing code - 9 -. 0 replaced by valid number
+	// NOTE! Valid tests 5 - to 7 refactored after implementing code - 10 -. 0 replaced by valid number
+	
+	@Test
+	// 2  - check for input contains only valid symbols
+	public void test_not_valid_symbols() throws Exception {
+		assertEquals("Input contains Not valid symbols!",-2, rn.convertToInteger("ZI"));
+		assertEquals("Input contains only valid symbols!",11, rn.convertToInteger("XI"));
+	}
+
+//	@Test
+//	// 3  - symbols I, X, C, and M can be repeated at most 3 times in a row
+//	public void test_not_valid_repeatingsIXCM() throws Exception {
+//		assertEquals("Input contains Not valid repeatings!",-3, rn.convertToInteger("XXXXI"));
+//		assertEquals("Input contains only valid repeatings!",0, rn.convertToInteger("XXXI"));
+//	}
+//
+//	@Test
+//	// 4  - symbols V, L, and D can never be repeated
+//	public void test_not_valid_repeatingsVLD() throws Exception {
+//		assertEquals("Input contains Not valid repeatings!",-4, rn.convertToInteger("XXXIVV"));
+//		assertEquals("Input contains only valid repeatings!",0, rn.convertToInteger("XXXIV"));
+//	}
+	 
+	// Test refactoring as well - 3 and 4 in one now
+	@Test
+	// 3  - symbols I, X, C, and M can be repeated at most 3 times in a row
+  	// 4  - symbols V, L, and D can never be repeated
+	public void test_not_valid_repetitions() throws Exception {
+		assertEquals("Input contains Not valid repeatings!",-3, rn.convertToInteger("XXXXI"));
+		assertEquals("Input contains only valid repeatings!",31, rn.convertToInteger("XXXI"));
+		assertEquals("Input contains Not valid repeatings!",-3, rn.convertToInteger("XXXVV"));
+		assertEquals("Input contains only valid repeatings!",35, rn.convertToInteger("XXXV"));
+	}
+	
+	// Test refactoring as follow - adding new asserts for each new condition
+	@Test
+	public void test_not_valid_subtractions() throws Exception {
+		// 5  - The '1' symbols (I, X, and C) can only be subtracted from the 2 next highest values
+	  	assertEquals("Input contains Not valid subtractions!",-4, rn.convertToInteger("IM"));
+		assertEquals("Input contains only valid subtractions!",4, rn.convertToInteger("IV"));
+		// 6  - Only one subtraction can be made per numeral (XC is allowed, XXC is	not)
+		assertEquals("Input contains Not valid subtractions!",-4, rn.convertToInteger("XXC"));
+		assertEquals("Input contains only valid subtractions!",90, rn.convertToInteger("XC"));
+		// 7  - The '5' symbols (V, L, and D) can never be subtracted
+		assertEquals("Input contains Not valid subtractions!",-4, rn.convertToInteger("VX"));
+	}
+
+	@Test
+	// 8  - check every single number
+	public void test_valid_single_number() throws Exception {
+		assertEquals("Not valid single number!",1, rn.convertToInteger("I"));
+		assertEquals("Not valid single number!",5, rn.convertToInteger("V"));
+		assertEquals("Not valid single number!",10, rn.convertToInteger("X"));
+		assertEquals("Not valid single number!",50, rn.convertToInteger("L"));
+		assertEquals("Not valid single number!",100, rn.convertToInteger("C"));
+		assertEquals("Not valid single number!",500, rn.convertToInteger("D"));
+		assertEquals("Not valid single number!",1000, rn.convertToInteger("M"));
+	}
+
+	@Test
+	// 9  - check valid numbers without subtraction
+	public void test_valid_without_subtractions() throws Exception {
+		assertEquals("Not valid number!",2, rn.convertToInteger("II"));
+		assertEquals("Not valid number!",6, rn.convertToInteger("VI"));
+		assertEquals("Not valid number!",12, rn.convertToInteger("XII"));
+		assertEquals("Not valid number!",60, rn.convertToInteger("LX"));
+	}
+
+	@Test
+	// 10  - check any valid numbers 
+	public void test_valid_any_number() throws Exception {
+		assertEquals("Not valid number!",45, rn.convertToInteger("XLV"));
+		assertEquals("Not valid number!",4, rn.convertToInteger("IV"));
+		assertEquals("Not valid number!",1984, rn.convertToInteger("MCMLXXXIV"));
+		assertEquals("Not valid number!",2014, rn.convertToInteger("MMXIV"));
+		assertEquals("Not valid number!",3999, rn.convertToInteger("MMMCMXCIX"));
+	}
+
+} 
